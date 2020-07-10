9b10b815b7b01896abad008e5fb9fffef0361bc2
Simon Nyström <simon.nystrom90@gmail.com>
2020-07-10 16:56:59 +0200

    updates

--- prompt.sh
+++ prompt.sh
@@ -1,6 +1,9 @@
 function git_prompt {
     if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) = true ]]; then
-        echo "%F{blue}(%f%F{red}$(git rev-parse --abbrev-ref HEAD)%f""%F{blue})%f"
+        local git_status=$(git status -s)
+        local modified="$(echo $git_status | wc -l)"
+        echo $modified
+        # echo "%F{blue}(%f%F{red}$(git rev-parse --abbrev-ref HEAD)%f""%F{blue})%f"
     fi
 }
 
@@ -12,4 +15,6 @@ if [[ "$EUID" -eq 0 ]]; then
     RPROMPT="%K{red} WARNING: Running as root %k"
 fi
 
-PROMPT="%F{cyan} %~ %B➜%b%f "
+# ✓
+# PROMPT="%F{#5ff967}%~ %f%F{#5ff967}%B➜%b%f "
+PROMPT="%F{#5ff967}%~ %f%F{#5ff967}%B➜%b%f "
